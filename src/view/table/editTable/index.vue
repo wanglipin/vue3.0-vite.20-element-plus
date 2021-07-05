1306719da53599439d24cbaf41e5a5d56b1c055f
o_wlp <wlpboyun.11.com>
2021-07-05 09:26:37 +0800

    fix:修改部分配置文件

--- src/view/table/editTable/index.vue
+++ src/view/table/editTable/index.vue
@@ -1,5 +1,12 @@
+<!--
+ * @Author: O_Pin
+ * @Date: 2021-07-01 13:57:16
+ * @LastEditTime: 2021-07-02 10:22:48
+-->
 <template>
-  <editTable :data="tableData" :columns="columns" border @selection-change="true"></editTable>
+  <el-card class="box-card">
+    <editTable :data="tableData" :columns="columns" border @selection-change="true"></editTable>
+  </el-card>
 </template>
 
 <script lang='ts'>
@@ -13,35 +20,17 @@ export default defineComponent ({
     const state = reactive({
       tableData: [
         {
-          id: '1',
+          id: '2',
           name: '王小虎',
           amount1: '234',
           amount2: '3.2',
           amount3: 10
         }, {
-          id: '1',
+          id: '2',
           name: '王小虎',
           amount1: '165',
           amount2: '4.43',
           amount3: 12
-        }, {
-          id: '2',
-          name: '王小虎',
-          amount1: '324',
-          amount2: '1.9',
-          amount3: 9
-        }, {
-          id: '2',
-          name: '王小虎',
-          amount1: '621',
-          amount2: '2.2',
-          amount3: 17
-        }, {
-          id: '2',
-          name: '王小虎',
-          amount1: '539',
-          amount2: '4.1',
-          amount3: 15
         }
       ],
       columns:[
--- src/view/table/editTable/table.tsx
+++ src/view/table/editTable/table.tsx
@@ -1,29 +1,31 @@
-import { defineComponent, reactive } from 'vue';
+
+import _Row from 'element-plus/lib/el-row';
+import { createSlots, defineComponent, reactive } from 'vue';
 
 export default defineComponent({
   inheritAttrs: false, // attrs 用这个值
   props: ['data', 'columns'],
-  setup(props, { attrs }) {
-    console.log(attrs, props.data) // 属性，方法都可以继承
+  setup(props, { slots, attrs }) {
     const state = reactive<any>({});
     return () => {
       return(
-        <el-card class="box-card">
-          <el-table
-            data={ props.data }
-            { ...attrs }>
-            {
-              props.data.map((item: any, index: number) => {
-                return (
-                  <el-table-column
-                  { ...props.columns[index] }
-                  >
-                  </el-table-column>
-                )
-              })
-            }
-          </el-table>
-      </el-card>
+        <el-table
+          data={ props.data }
+          { ...attrs }>
+          {
+            props.columns.map((item: any) => {
+              return (
+                <el-table-column
+                { ...item }
+                >
+                  {{
+                    default: () => <span>{(item)}</span>
+                  }}
+                </el-table-column>
+              )
+            })
+          }
+        </el-table>
       )
     }
   }
